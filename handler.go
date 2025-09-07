e15ca0df67ae720ad6d99237457a4d29d8512fa9
Mikhail <kocmoc777-88@mail.ru>
2025-09-07 13:12:49 +0500

    2

--- handler.go
+++ handler.go
@@ -16,7 +16,7 @@ func NewHalloHandler(router *http.ServeMux) {
 
 func (handler *HalloHandler) Hello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		randomNumber := rand.Intn(100)
+		randomNumber := rand.Intn(6)
 		strNumber := strconv.Itoa(randomNumber)
 		fmt.Println(randomNumber)
 		w.Write([]byte(strNumber))
